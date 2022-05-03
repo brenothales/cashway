ef8b2f8836ecbdd90345d3dbbb9a7f336fa3c88c
Breno Thales <breno.thales@gmail.com>
2022-05-03 19:45:26 -0300

    Ajuste no feedback

--- banking/start.sh
+++ banking/start.sh
@@ -1,7 +1,7 @@
 # Instala as Gems
 bundle check || bundle install
 # Roda nosso servidor
+figlet -c Banking CashWay
 bundle exec puma -C ./config/puma.rb
 # Roda os jobs
 bundle exec sidekiq -q emails
-figlet -c Banking - CashWay
--- gerencial/start.sh
+++ gerencial/start.sh
@@ -2,6 +2,6 @@
 bundle check || bundle install
 # Roda nosso servidor
 bundle exec rails db:create db:migrate db:seed
+figlet -c Gerencial CashWay
 bundle exec puma -C ./config/puma.rb
-figlet -c Gerencial
 
