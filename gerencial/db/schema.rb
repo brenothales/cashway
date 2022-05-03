2fde64664e5d7070881d2243a5895bb209d8d31d
Breno Thales <breno.thales@gmail.com>
2022-05-03 20:23:25 -0300

    Removendo arquivos desnecessarios

--- gerencial/db/schema.rb
+++ gerencial/db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 20220502183305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.string   "autor"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                                  null: false
     t.string   "encrypted_password",                     null: false
