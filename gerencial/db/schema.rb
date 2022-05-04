8e9ac7fcadcd59d63186e744490e909ff9454b40
Breno Thales <breno.thales@gmail.com>
2022-05-04 09:42:22 -0300

    Ajustes

--- gerencial/db/schema.rb
+++ gerencial/db/schema.rb
@@ -16,16 +16,16 @@ ActiveRecord::Schema.define(version: 20220502183305) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                  null: false
-    t.string   "encrypted_password",                     null: false
-    t.string   "name",                                   null: false
-    t.integer  "account",                                null: false
+    t.string   "email",                                                                                                       null: false
+    t.string   "encrypted_password",                                                                                          null: false
+    t.string   "name",                                                                                                        null: false
+    t.bigint   "account",                                                                                                     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "message"
+    t.datetime "created_at",                                                                                                  null: false
+    t.datetime "updated_at",                                                                                                  null: false
+    t.string   "message",                default: "Cartão de crédito com anuidade zero. Conta digital prática e sem custos."
     t.boolean  "admin",                  default: false
     t.index ["account"], name: "index_users_on_account", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
