138f3312feeccb106ddaa2d6389fdcddf53a81f3
Breno Thales <breno.thales@gmail.com>
2022-05-04 09:25:05 -0300

    Removendo arquivos desnecessarios

--- gerencial/app/models/user.rb
+++ gerencial/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :account, :message, :name, presence: true
-  validates :account, uniqueness: true
+  validates :account, uniqueness: true, length: {minimum: 5, maximum: 8}, allow_blank: false
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
   def email_required?
--- gerencial/start.sh
+++ gerencial/start.sh
@@ -2,6 +2,7 @@
 bundle check || bundle install
 # Roda nosso servidor
 bundle exec rails db:create db:migrate db:seed
+bundle exec rails assets:clean assets:clobber assets:precompile
 figlet -c Gerencial CashWay
 bundle exec puma -C ./config/puma.rb
 
